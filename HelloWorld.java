ab1b20b387b7ad97f95a92b30d91a13551239a5b
root <root@ip-172-31-18-164.us-east-2.compute.internal>
2021-08-18 20:07:37 +0000

    commit from feature101 branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java 1.8");  
+     System.out.println("Hello Java 1.8 from feature101 branch");  
     }  
 }  
